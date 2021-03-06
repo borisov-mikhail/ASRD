757c02c0509c783fdf080e0c685cb5a023bc2040
Mikhail Borisov <travka_mx@mail.ru>
2021-03-07 01:15:03 +0300

    Start refactor parser of samples

--- asrd/data_preparation.py
+++ asrd/data_preparation.py
@@ -1,8 +1,83 @@
+from dataclasses import dataclass
+from typing import List
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 sns.set_theme(color_codes=True)
+
+
+@dataclass
+class SamplePoint:
+    # target: str
+    start: str
+    # time: str
+    # finish_time: str
+    # p_p0: str  # P / P0
+    # p_p1: str  # P / P0_1
+    # peak_start: str
+    # peak_finish: str
+    # pick_max: str
+    # pick_amplitude: str
+    # S_of_pick: str
+    # grad_koeff: str
+
+
+@dataclass
+class Sample:
+    create_time: str
+    # sample_name: str
+    # operator: str
+    # mass: str
+    # vlazhnost: str
+    # atmosphere_pressure: str
+    # atmosphere_pressure_1: str
+    # graduation_name: str
+    # graduation_time: str
+    # summarnyy_raskhod: str
+    # idk: str
+    # idk1: str
+    # temperature: str  # "T, K"
+    # density: str
+    # l_to_d: str  # "L/D"
+
+    points: List[SamplePoint]
+
+
+class Analyzer:
+    samples: List[Sample] = []
+
+    def parse(self, path):
+        with open(path, 'r', encoding='windows-1251') as file:
+            content = file.read().strip()
+
+        for line in content.split('\n'):
+            columns = line.split(',')
+
+            if columns[0] == 'SAMPLE':
+                current_sample = Sample(
+                    create_time=columns[1],
+                    points=[]
+                )
+
+                self.samples.append(current_sample)
+            elif columns[0] == 'POINT':
+                current_point = SamplePoint(
+                    start=columns[1]
+                )
+
+                self.samples[-1].points.append(current_point)
+            else:
+                raise Exception(f'{columns[0]} not found')
+
+    def plot_graph(self, index):
+        sample = self.samples[index]
+
+
+analyzer = Analyzer()
+analyzer.parse('/home/mikhail/Development/asrd/examples_data/full_isoterms.srb')
 
 
 def read_data(path):
@@ -31,44 +106,44 @@ def read_data(path):
     return np.array(samples.sample_name)
 
 
-def plot_graph(index_of_sample, x, y):
+def plot_graph(sample: Sample):
     pass
     # current_index = indexes[index_of_sample] + 1:indexes[index_of_sample + 1]
     # data = submissions_data[current_index]
     # sns.set_style("ticks", {'xtick.color': '.0', 'ytick.color': '.0'})
     # g = sns.lmplot(
-    #   x=x,
-    #   y=y,
-    #   data=data,
-    #   hue='0_1',
-    #   height=3,
-    #   aspect=4,
-    #   legend=0,
-    #   fit_reg=False,
-    #   scatter_kws={"s": 50}
+    #     x=x,
+    #     y=y,
+    #     data=data,
+    #     hue='0_1',
+    #     height=3,
+    #     aspect=4,
+    #     legend=0,
+    #     fit_reg=False,
+    #     scatter_kws={"s": 50}
     # )
     # plt.title(
-    #   "Изотерма адсорбции",
-    #   fontsize=18,
-    #   bbox=dict(edgecolor='black', color='w'),
-    #   horizontalalignment='center'
+    #     "Изотерма адсорбции",
+    #     fontsize=18,
+    #     bbox=dict(edgecolor='black', color='w'),
+    #     horizontalalignment='center'
     # )
     # plt.ylabel(
-    #   '$V, mm^{3}$',
-    #   rotation=0,
-    #   fontsize=14,
-    #   verticalalignment='top',
-    #   horizontalalignment='right'
+    #     '$V, mm^{3}$',
+    #     rotation=0,
+    #     fontsize=14,
+    #     verticalalignment='top',
+    #     horizontalalignment='right'
     # )
     # plt.xlabel('$p/p_{0}$', fontsize=16)
     # plt.xticks(np.arange(0, 1.1, step=0.1), fontsize=13)
     # plt.yticks(fontsize=13)
     # plt.grid(
-    #   True,
-    #   which=u'major',
-    #   color='black',
-    #   linewidth=1.,
-    #   linestyle='-'
+    #     True,
+    #     which=u'major',
+    #     color='black',
+    #     linewidth=1.,
+    #     linestyle='-'
     # )
     # path = os.getcwd() + "\\data\\" + x + y + ".jpg"
     # g.savefig(path)
