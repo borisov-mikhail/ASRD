3f3e1cc533cc8d25afa7a15acc1e9b48619de9b6
Mikhail Borisov <travka_mx@mail.ru>
2021-03-11 01:00:31 +0300

    Add uploading form

--- asrd/app.py
+++ asrd/app.py
@@ -1,14 +1,53 @@
-from flask import Flask, render_template, send_from_directory
+import os
+from flask import Flask, render_template, send_from_directory, request, flash,\
+    redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+ALLOWED_EXTENSIONS = {'srb'}
+app.secret_key = "secret key"
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', graph='test')
+    return render_template('index.html')
 
 
 @app.route('/plot/<name>')
 def send_plot(name):
     return send_from_directory('../data', name)
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return render_template('index.html', graph='test')
+        else:
+            flash('Allowed file types are srb')
+            return redirect(request.url)
